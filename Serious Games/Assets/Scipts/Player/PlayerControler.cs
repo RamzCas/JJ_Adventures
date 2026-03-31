df277de032260638b0b3dedbf2522d1351cbea73
RamzCas <2749414@students.wits.ac.za>
2026-03-31 10:39:54 +0200

    character

--- Serious Games/Assets/Scipts/Player/PlayerControler.cs
+++ Serious Games/Assets/Scipts/Player/PlayerControler.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement")]
+    public float Speed;
+    public float Rotation;
+
+    [Header("Interation")]
+    public float InteractRange;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
