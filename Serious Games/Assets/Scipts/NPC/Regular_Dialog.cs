a2a3e8033af7ce616914c26c7e62c6457a71e6d7
RamzCas <2749414@students.wits.ac.za>
2026-07-25 09:18:58 +0200

    fixing up some stuff

---
+++ Serious Games/Assets/Scipts/NPC/Regular_Dialog.cs
@@ -0,0 +1,131 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Regular_Dialog : MonoBehaviour
+{
+    [Header("Components")]
+    public GameObject InteractPt;
+    //public GameObject[] DialogBoards;//Some reasone we need to add an etra point to work 
+    public string[] DialogLines;
+
+    public TextMeshProUGUI TextMeshPro;
+    public GameObject Canvas;
+
+
+
+    [Header("Array")]
+    public int NumberOfDialog;
+    public int CurrentNumberOfDialog;
+
+    [Header("Bools")]
+    public bool InConvo;
+
+    [Header("Other Scripts")]
+    public PlayerController PlayerController;
+
+
+    PlayerControler Controls;
+
+    private void Awake()
+    {
+        Controls = new PlayerControler();
+
+        NumberOfDialog = DialogLines.Length;
+    }
+
+    private void OnEnable()
+    {
+        Controls.Enable();
+        Controls.Player.Dialog.performed += DialogControls;
+    }
+
+    private void OnDisable()
+    {
+        Controls.Player.Dialog.performed -= DialogControls;
+        Controls.Disable();
+    }
+
+
+
+
+    private void Update()
+    {
+        
+            NumberOfDialog = DialogLines.Length;
+        
+
+
+        if (CurrentNumberOfDialog >= NumberOfDialog - 1f)
+        {
+
+            InteractPt.SetActive(true);
+            InConvo = false;
+        }
+    }
+
+
+
+
+    private void FixedUpdate()
+    {
+        if (!InteractPt.activeSelf)
+        {
+            //Debug.Log("Interated with ncp: Start dilog");
+            InConvo = true;
+
+        }
+
+
+        //chaning the dialog system 
+        if (InConvo)
+        {
+            /*DialogBoards[CurrentNumberOfDialog].gameObject.SetActive(true);
+            PlayerController.CanMove = false;
+            Canvas.SetActive(true);*/
+
+            Canvas.SetActive(true);
+            PlayerController.CanMove = false;
+
+           
+            TextMeshPro.text = DialogLines[CurrentNumberOfDialog];
+            
+
+
+        }
+
+        if (!InConvo)
+        {
+            PlayerController.CanMove = true;
+            Canvas.SetActive(false);
+            CurrentNumberOfDialog = 0;
+        }
+
+
+        if (CurrentNumberOfDialog >= NumberOfDialog - 1f)
+        {
+
+            //Debug.Log("EndConvo");
+            InteractPt.SetActive(true);
+            InConvo = false;
+        }
+
+
+    }
+
+
+
+    public void DialogControls(InputAction.CallbackContext context)
+    {
+        if (InConvo)
+        {
+            if (context.performed)
+            {
+                Debug.Log("Convo Progress");
+                CurrentNumberOfDialog++;
+            }
+        }
+
+    }
+}
+
---
+++ Serious Games/Assets/Scipts/World/Game_Manager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    [Header("Stamps")]
+    public int maxStamps;
+    public int currentStamps;
+
+
+}
