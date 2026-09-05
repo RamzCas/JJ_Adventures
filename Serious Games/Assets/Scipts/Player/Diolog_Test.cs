dd209a8c86772496c7a5b9dbf6366b352defb0c4
RamzCas <2749414@students.wits.ac.za>
2026-09-05 15:17:31 +0200

    fixing the dialog

--- Serious Games/Assets/Scipts/Player/Diolog_Test.cs
+++ Serious Games/Assets/Scipts/Player/Diolog_Test.cs
@@ -8,20 +8,22 @@ public class Diolog_Test : MonoBehaviour
 {
     [Header("Components")]
     public GameObject InteractPt;
-    //public GameObject[] DialogBoards;//Some reasone we need to add an etra point to work 
     public string[] DialogLines;
 
     public TextMeshProUGUI characterNameCanvas;
     public string characterName;
-  
+
     public TextMeshProUGUI TextMeshPro;
     public GameObject Canvas;
-  
-   
 
-    [Header("Array")]
-    public int NumberOfDialog;
-    public int CurrentNumberOfDialog;
+    [Header("Player Chats")]
+    public GameObject playerCanvas;
+    public TextMeshProUGUI playerText;
+    public string[] PlayerDialogYap;
+
+    [Header("Info")]
+    public int NumberOfDialog;       // combined length, informational only
+    public int CurrentNumberOfDialog; // total lines shown so far (npc + player)
 
     [Header("Bools")]
     public bool InConvo;
@@ -29,43 +31,29 @@ public class Diolog_Test : MonoBehaviour
     [Header("Other Scripts")]
     public PlayerController PlayerController;
     public QuestManager QuestManager;
-    /*public Start_Fetch_Qeust Start_Fetch_Quest;
-    public FInished_Fetch_Quest Finished_Fetch_Quest;*/
 
     [Header("Quest Settings")]
     public bool giverOfQuest;
     public bool QuestNPC;
     public bool enderOfQuest;
     public string[] questDialog;
+    public string[] PlayerQuestYap;
 
+    private PlayerControler Controls;
+    private bool wasInConvo;
 
-    PlayerControler Controls;
+    // Independent progress trackers - this is what actually decides when the
+    // conversation is over: both must be exhausted, regardless of which
+    // array is longer.
+    private int npcIndex;
+    private int playerIndex;
+    private bool npcTurnNext = true;
 
     private void Awake()
     {
         characterNameCanvas.text = characterName;
         Controls = new PlayerControler();
-        //NumberOfDialog = DialogBoards.Length;
-        //NumberOfDialog = DialogLines.Length;
-
-        if (QuestNPC) 
-        {
-            if (QuestManager.startQuest)
-            {
-                NumberOfDialog = questDialog.Length;
-            }
-
-            else
-            {
-                NumberOfDialog = DialogLines.Length;
-            }
-        }
-
-        else 
-        {
-            NumberOfDialog = DialogLines.Length;
-        }
-       
+        RefreshDialogCount();
     }
 
     private void OnEnable()
@@ -80,157 +68,111 @@ public class Diolog_Test : MonoBehaviour
         Controls.Disable();
     }
 
-
-
-
     private void Update()
     {
-        if (QuestManager.startQuest && enderOfQuest) 
-        {
-            NumberOfDialog = questDialog.Length;
-        }
+        RefreshDialogCount();
 
-        if (!QuestManager.startQuest && enderOfQuest)
-        {
-            NumberOfDialog = DialogLines.Length;
-        }
+        var (npcLines, playerLines) = GetActiveDialogArrays();
+        bool convoFinished = npcIndex >= npcLines.Length && playerIndex >= playerLines.Length;
 
-        if (QuestManager.startQuest && giverOfQuest) 
-        {
-            NumberOfDialog = questDialog.Length;
-        }
-
-        if (!QuestManager.startQuest && giverOfQuest)
-        {
-            NumberOfDialog = DialogLines.Length;
-        }
-
-
-        if (CurrentNumberOfDialog >= NumberOfDialog - 1f && enderOfQuest && QuestManager.startQuest)
+        if (convoFinished && enderOfQuest && QuestManager.startQuest)
         {
             QuestManager.startQuest = false;
             QuestManager.stopQuest = true;
-            InteractPt.SetActive(true);
-            //PlayerController.CurrentSpeed = PlayerController.Speed;
-            InConvo = false;
-        }
-    }
-
-
-
-
-    private void FixedUpdate()
-    {
-        if(!InteractPt.activeSelf) 
-        {
-            //Debug.Log("Interated with ncp: Start dilog");
-            InConvo = true;
-            
         }
 
-        
-        //chaning the dialog system 
-        if(InConvo) 
-        {
-            /*DialogBoards[CurrentNumberOfDialog].gameObject.SetActive(true);
-            PlayerController.CanMove = false;
-            Canvas.SetActive(true);*/
-
-            Canvas.SetActive(true);
-            PlayerController.CanMove = false;
-            PlayerController.CurrentSpeed = 0;
-
-            /*if(QuestManager.startQuest && QuestNPC) 
-            {
-                TextMeshPro.text = questDialog[CurrentNumberOfDialog];
-            }
-
-            
-            if(QuestManager.startQuest && giverOfQuest) 
-            {
-                TextMeshPro.text += questDialog[CurrentNumberOfDialog];
-            }
-
-            else
-            {
-                TextMeshPro.text = DialogLines[CurrentNumberOfDialog];
-            }*/
-
-            if (QuestManager.startQuest && enderOfQuest)
-            {
-                //NumberOfDialog = questDialog.Length;
-                TextMeshPro.text = questDialog[CurrentNumberOfDialog];
-            }
-
-            if(!QuestManager.startQuest && enderOfQuest) 
-            {
-                TextMeshPro.text = DialogLines[CurrentNumberOfDialog];
-            }
-
-            if (QuestManager.startQuest && giverOfQuest)
-            {
-                //NumberOfDialog = questDialog.Length;
-                TextMeshPro.text = questDialog[CurrentNumberOfDialog];
-            }
-
-            if (!QuestManager.startQuest && giverOfQuest)
-            {
-                //NumberOfDialog = DialogLines.Length;
-                TextMeshPro.text = DialogLines[CurrentNumberOfDialog];
-            }
-
-        }
-
-        if (!InConvo) 
-        {
-            PlayerController.CanMove = true;
-            Canvas.SetActive(false);
-            CurrentNumberOfDialog = 0;
-            //NumberOfDialog = DialogLines.Length;
-            PlayerController.CurrentSpeed = PlayerController.Speed;
-        }
-
-
-        if(CurrentNumberOfDialog >= NumberOfDialog - 1f) 
-        {
-            
-            //Debug.Log("EndConvo");
-            InteractPt.SetActive(true);
-            InConvo = false;
-            //PlayerController.CurrentSpeed = PlayerController.Speed;
-        }
-
-
-        if (CurrentNumberOfDialog >= NumberOfDialog - 1f && giverOfQuest)
+        if (convoFinished && giverOfQuest)
         {
             QuestManager.startQuest = true;
-
             Debug.Log("Start Quest");
         }
 
-        /*if (CurrentNumberOfDialog >= NumberOfDialog - 1f && enderOfQuest && QuestManager.startQuest)
+        if (convoFinished)
         {
-            QuestManager.startQuest = false;
-            QuestManager.stopQuest = true;
-           
-            //QuestNPC = false;
-        }*/
+            InteractPt.SetActive(true);
+            InConvo = false;
+        }
 
-      
+        if (!InteractPt.activeSelf)
+        {
+            InConvo = true;
+        }
+
+        if (InConvo && !wasInConvo)
+        {
+            // Just entered conversation this frame - show the opening line.
+            Canvas.SetActive(true);
+            playerCanvas.SetActive(true);
+            PlayerController.CanMove = false;
+            PlayerController.CurrentSpeed = 0;
+            AdvanceDialog();
+        }
+        else if (!InConvo)
+        {
+            PlayerController.CanMove = true;
+            Canvas.SetActive(false);
+            playerCanvas.SetActive(false);
+            CurrentNumberOfDialog = 0;
+            npcIndex = 0;
+            playerIndex = 0;
+            npcTurnNext = true;
+            PlayerController.CurrentSpeed = PlayerController.Speed;
+        }
+
+        wasInConvo = InConvo;
     }
-
-
 
     public void DialogControls(InputAction.CallbackContext context)
     {
-        if(InConvo) 
+        if (InConvo && context.performed)
         {
-            if (context.performed)
-            {
-                Debug.Log("Convo Progress");
-                CurrentNumberOfDialog++;
-            }
+            Debug.Log("Convo Progress");
+            AdvanceDialog();
         }
-     
+    }
+
+    // Shows the next line, alternating npc/player when both still have lines
+    // left. If one side has run out, all remaining turns go to whichever
+    // side still has lines, so the conversation never ends early - it only
+    // stops once BOTH arrays are exhausted.
+    private void AdvanceDialog()
+    {
+        var (npcLines, playerLines) = GetActiveDialogArrays();
+        bool npcHasMore = npcIndex < npcLines.Length;
+        bool playerHasMore = playerIndex < playerLines.Length;
+
+        if (!npcHasMore && !playerHasMore)
+            return;
+
+        bool npcGoesNow = npcHasMore && (npcTurnNext || !playerHasMore);
+
+        if (npcGoesNow)
+        {
+            TextMeshPro.text = npcLines[npcIndex];
+            npcIndex++;
+            npcTurnNext = false;
+        }
+        else if (playerHasMore)
+        {
+            playerText.text = playerLines[playerIndex];
+            playerIndex++;
+            npcTurnNext = true;
+        }
+
+        CurrentNumberOfDialog++;
+    }
+
+    private (string[] npcLines, string[] playerLines) GetActiveDialogArrays()
+    {
+        bool useQuestDialog = QuestNPC && QuestManager.startQuest && (giverOfQuest || enderOfQuest);
+        return useQuestDialog
+            ? (questDialog, PlayerQuestYap)
+            : (DialogLines, PlayerDialogYap);
+    }
+
+    private void RefreshDialogCount()
+    {
+        var (npcLines, playerLines) = GetActiveDialogArrays();
+        NumberOfDialog = npcLines.Length + playerLines.Length;
     }
 }
